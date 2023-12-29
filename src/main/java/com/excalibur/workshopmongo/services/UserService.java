fe9760f05521d6181add9b9948e8546d816c1790
Arthur Queiroz <arthutqueiroz@hotmail.com>
2023-12-29 17:15:09 -0300

    User findById

--- src/main/java/com/excalibur/workshopmongo/services/UserService.java
+++ src/main/java/com/excalibur/workshopmongo/services/UserService.java
@@ -1,13 +1,14 @@
 package com.excalibur.workshopmongo.services;
 
 import com.excalibur.workshopmongo.domain.User;
-
 import com.excalibur.workshopmongo.repository.UserRepository;
+import com.excalibur.workshopmongo.services.exception.ObjectNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -17,5 +18,10 @@ public class UserService {
 
     public List<User> findAll(){
         return repo.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
     }
 }
