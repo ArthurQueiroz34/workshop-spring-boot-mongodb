fc684559b91c5b05509ec8eaf372744b5b2e9da7
Arthur Queiroz <arthutqueiroz@hotmail.com>
2023-12-29 17:23:24 -0300

    Inserting User with POST

--- src/main/java/com/excalibur/workshopmongo/services/UserService.java
+++ src/main/java/com/excalibur/workshopmongo/services/UserService.java
@@ -1,6 +1,7 @@
 package com.excalibur.workshopmongo.services;
 
 import com.excalibur.workshopmongo.domain.User;
+import com.excalibur.workshopmongo.dto.UserDTO;
 import com.excalibur.workshopmongo.repository.UserRepository;
 import com.excalibur.workshopmongo.services.exception.ObjectNotFoundException;
 
@@ -23,5 +24,13 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+    }
+
+    public User insert(User obj) {
+        return repo.insert(obj);
+    }
+
+    public User fromDTO(UserDTO objDto) {
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
