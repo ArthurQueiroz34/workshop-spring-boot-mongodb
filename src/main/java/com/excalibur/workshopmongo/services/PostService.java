fe39dc1043ba18739a16b2f8852044731e22ed53
Arthur Queiroz <arthutqueiroz@hotmail.com>
2023-12-31 14:33:10 -0300

    Query with Multiple Criteria

--- src/main/java/com/excalibur/workshopmongo/services/PostService.java
+++ src/main/java/com/excalibur/workshopmongo/services/PostService.java
@@ -7,6 +7,7 @@ import com.excalibur.workshopmongo.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.Optional;
 import java.util.List;
 
@@ -23,5 +24,10 @@ public class PostService {
 
     public List<Post> findByTitle(String text) {
         return repo.searchTitle(text);
+    }
+
+    public List<Post> fullSearch(String text, Date minDate, Date maxDate) {
+        maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+        return repo.fullSearch(text, minDate, maxDate);
     }
 }
