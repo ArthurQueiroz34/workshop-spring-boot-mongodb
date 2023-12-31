11a3abed4dcc84b040d8c8668856b0b1b23b8605
Arthur Queiroz <arthutqueiroz@hotmail.com>
2023-12-31 13:52:02 -0300

    Endpoint to retrieve posts for User

--- src/main/java/com/excalibur/workshopmongo/resources/UserResource.java
+++ src/main/java/com/excalibur/workshopmongo/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.excalibur.workshopmongo.resources;
 
+import com.excalibur.workshopmongo.domain.Post;
 import com.excalibur.workshopmongo.domain.User;
 import com.excalibur.workshopmongo.dto.UserDTO;
 import com.excalibur.workshopmongo.services.UserService;
@@ -57,5 +58,11 @@ public class UserResource {
         obj.setId(id);
         obj = service.update(obj);
         return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(value="/{id}/posts", method=RequestMethod.GET)
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj.getPosts());
     }
 }
