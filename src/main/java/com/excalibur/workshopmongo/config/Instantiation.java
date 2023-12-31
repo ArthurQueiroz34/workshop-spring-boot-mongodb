63aef11795a6799c06fd5bd4451e76003dc3bda1
Arthur Queiroz <arthutqueiroz@hotmail.com>
2023-12-31 14:14:49 -0300

    Adding comments to posts

--- src/main/java/com/excalibur/workshopmongo/config/Instantiation.java
+++ src/main/java/com/excalibur/workshopmongo/config/Instantiation.java
@@ -3,6 +3,7 @@ package com.excalibur.workshopmongo.config;
 import com.excalibur.workshopmongo.domain.Post;
 import com.excalibur.workshopmongo.domain.User;
 import com.excalibur.workshopmongo.dto.AuthorDTO;
+import com.excalibur.workshopmongo.dto.CommentDTO;
 import com.excalibur.workshopmongo.repository.PostRepository;
 import com.excalibur.workshopmongo.repository.UserRepository;
 
@@ -38,6 +39,13 @@ public class Instantiation implements CommandLineRunner {
 
         Post post1 = new Post(null, sdf.parse("21/03/2018"), "Wise reminder", "Let the fire of your heart burn bright!", new AuthorDTO(alexia));
         Post post2 = new Post(null, sdf.parse("23/03/2018"), "Never forget!", "Life itself is with you.", new AuthorDTO(alexia));
+
+        CommentDTO c1 = new CommentDTO("Always let the light beacon", sdf.parse("21/03/2018"), new AuthorDTO(sylnas));
+        CommentDTO c2 = new CommentDTO("That's one way to live by", sdf.parse("22/03/2018"), new AuthorDTO(sylnas));
+        CommentDTO c3 = new CommentDTO("One must always remember", sdf.parse("23/03/2018"), new AuthorDTO(sylnas));
+
+        post1.getComments().addAll(Arrays.asList(c1, c2));
+        post2.getComments().addAll(Arrays.asList(c3));
 
         userRepository.saveAll(Arrays.asList(alexia, sylnas, yacina));
         postRepository.saveAll(Arrays.asList(post1, post2));
